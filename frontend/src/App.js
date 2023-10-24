a8d1be99705430fc14d3710517fe707d1e192a5d
Shreya Mathur <smathur245@gmail.com>
2023-10-24 20:52:49 +0530

    Connected with frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,43 +1,117 @@
-import { useState, useEffect } from "react";
-import BarChart from "./components/BarChart";
-import { UserData } from './data'
+import { useEffect, useState } from "react";
+import "./App.css";
+import PieChart from "./components/PieChart";
+import { wait } from "@testing-library/user-event/dist/utils";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  let [question, setQuestion] = useState({});
+  const [options, setOptions] = useState([]);
+  const [myData, setMyData] = useState({})
+  const request1 = fetch('http://localhost:1234/api/6533c4490e9c16c7171df789/showQuestion').then(response => response.json());
+  const request2 = fetch('http://localhost:1234/api/6533c4490e9c16c7171df789/showOptions').then(response => response.json());
+  useEffect(() => {
+    Promise.all([request1, request2])
+      .then(([data1, data2]) => {
+        setQuestion(data1);
+        setOptions(data2);
+        setLoading(false);
 
 
-  const [userData, setUserData] = useState({
-    labels: [10, 20, 40, 80],
-    datasets: [
-      {
-        label: "Users Gained",
-        data: [1, 5, 10, 20],
-        backgroundColor: [
-          "rgba(75,192,192,1)",
-          "#ecf0f1",
-          "#50AF95",
-          "#f3ba2f",
-          "#2a71d0",
-        ],
-        borderColor: "black",
-        borderWidth: 2,
-      },
-    ],
-  });
-
-  console.log(UserData)
+      })
+      .catch(error => {
+        console.error(error);
+      });
 
 
+      
+    setMyData({
+      labels: options?.map((o) => (o.title)),
+      datasets: [
+        {
+          label: 'Votes',
+          data: options?.map((o) => (o.votes)),
+        },
+      ]
+
+    })
+
+
+    console.log(options?.map((o) => (o.title)))
+  }, []);
+
+
+
+
+
+  // QUESTION
+
+
+  // useEffect(() => {
+  //   fetch("http://localhost:1234/api/6533c4490e9c16c7171df789/showQuestion")
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       setQuestion(data);
+  // setLoading(false);
+  //     }
+
+  //     )
+  // }, []);
+
+
+
+  // //OPTIONS
+
+
+  // useEffect(() => {
+  //   fetch("http://localhost:1234/api/6533c4490e9c16c7171df789/showOptions")
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       setOptions(data);
+  //       setLoading(false);
+  //     })
+  // }, []);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  if (loading) {
+    return (
+      <div>Loading...</div>
+    )
+  }
 
   return (
     <div className="App">
+      <div className="Chart-heading">{question.title}</div>
+      {/* <div>{console.log(Array.isArray(options?.map((o) => (o.title)))) }</div> */}
+      {console.log(options?.map((o) => (o.title)))}
+      {console.log(options?.map((o) => (o.title)))}
+      console.log(options.title)
+
+
+      
       <div style={{ width: 700 }}>
-        <BarChart chartData={userData} />
+        <PieChart chartData={myData} />
       </div>
     </div>
   );
-
-  
-
 }
 
-export default App
+export default App;
