66781eb3bdf4b78e571c147adbac759b0bf660ac
Shreya Mathur <smathur245@gmail.com>
2023-12-19 11:56:58 +0530

    fixed frontend loading on refresh issue

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,112 +4,72 @@ import PieChart from "./components/PieChart";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  let [question, setQuestion] = useState({});
+  const [question, setQuestion] = useState({});
   const [options, setOptions] = useState([]);
-  const [myData, setMyData] = useState({})
-  const request1 = fetch('http://localhost:1234/api/6533c4490e9c16c7171df789/showQuestion').then(response => response.json());
-  const request2 = fetch('http://localhost:1234/api/6533c4490e9c16c7171df789/showOptions').then(response => response.json());
+  const [myData, setMyData] = useState({});
+
   useEffect(() => {
-    Promise.all([request1, request2])
-      .then(([data1, data2]) => {
+    const fetchData = async () => {
+      try {
+        const response1 = await fetch('http://localhost:1234/api/6533c7f51abc2cb921aed63d/showQuestion');
+        const data1 = await response1.json();
         setQuestion(data1);
+
+        const response2 = await fetch('http://localhost:1234/api/6533c7f51abc2cb921aed63d/showOptions');
+        const data2 = await response2.json();
         setOptions(data2);
+
         setLoading(false);
-
-
-      })
-      .catch(error => {
+      } catch (error) {
         console.error(error);
-      });
+        setLoading(false);
+      }
+    };
 
-
-      
-    setMyData({
-      labels: options?.map((o) => (o.title)),
-      datasets: [
-        {
-          label: 'Votes',
-          data: options?.map((o) => (o.votes)),
-          backgroundColor: [
-            'rgba(255, 26, 104, 0.5)',
-            'rgba(54, 162, 235, 0.5)',
-            'rgba(255, 206, 86, 0.5)',
-            'rgba(75, 192, 192, 0.5)',
-            'rgba(153, 102, 255, 0.5)',
-            'rgba(255, 159, 64, 0.5)',
-            'rgba(0, 0, 0, 0.5)'
-          ],
-          borderColor: [
-            'rgba(255, 26, 104, 1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)',
-            'rgba(0, 0, 0, 1)'
-          ],
-          borderWidth: 1
-        },
-      ]
-
-    })
-
-
-    console.log(options?.map((o) => (o.title)))
+    fetchData();
   }, []);
 
-
-
-
-
-  // QUESTION
-
-
-  // useEffect(() => {
-  //   fetch("http://localhost:1234/api/6533c4490e9c16c7171df789/showQuestion")
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       setQuestion(data);
-  // setLoading(false);
-  //     }
-
-  //     )
-  // }, []);
-
-
-
-  // //OPTIONS
-
-
-  // useEffect(() => {
-  //   fetch("http://localhost:1234/api/6533c4490e9c16c7171df789/showOptions")
-  //     .then(response => response.json())
-  //     .then(data => {
-  //       setOptions(data);
-  //       setLoading(false);
-  //     })
-  // }, []);
-
-
-
-
+  useEffect(() => {
+    if (options.length > 0) {
+      setMyData({
+        labels: options.map((o) => o.title),
+        datasets: [
+          {
+            label: 'Votes',
+            data: options.map((o) => o.votes),
+            backgroundColor: [
+              'rgba(255, 26, 104, 0.5)',
+              'rgba(54, 162, 235, 0.5)',
+              'rgba(255, 206, 86, 0.5)',
+              'rgba(75, 192, 192, 0.5)',
+              'rgba(153, 102, 255, 0.5)',
+              'rgba(255, 159, 64, 0.5)',
+              'rgba(0, 0, 0, 0.5)'
+            ],
+            borderColor: [
+              'rgba(255, 26, 104, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)',
+              'rgba(0, 0, 0, 1)'
+            ],
+            borderWidth: 1
+          },
+        ]
+      });
+    }
+  }, [options]);
 
   if (loading) {
-    return (
-      <div>Loading...</div>
-    )
+    return <div>Loading...</div>;
   }
 
   return (
     <div className="App">
       <div className="Chart-heading">{question.title}</div>
-      <div>{console.log(Array.isArray(options?.map((o) => (o.title)))) }</div>
-      {console.log(options?.map((o) => (o.title)))}
-      {console.log(options?.map((o) => (o.title)))}
-      
-
-
-      
+      <div>{console.log(Array.isArray(options.map((o) => o.title)))}</div>
       <div className="chart" style={{ width: 600 }}>
         <PieChart chartData={myData} />
       </div>
@@ -118,3 +78,4 @@ function App() {
 }
 
 export default App;
+
